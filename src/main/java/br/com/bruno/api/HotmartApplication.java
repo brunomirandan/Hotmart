9b572dc2f1665db5feb5f355005d76ff2d25961e
Bruno Miranda <brunomiranda.n@gmail.com>
2021-01-29 18:44:00 -0300

    Versão inicial do projeto (classes e mapeamento)

---
+++ src/main/java/br/com/bruno/api/HotmartApplication.java
@@ -0,0 +1,13 @@
+package br.com.bruno.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotmartApplication.class, args);
+	}
+
+}
